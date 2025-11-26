cbe77e45204c66e2352390e2a7ecf597dc3d0323
khalil1363 <fray.khalil.200@gmail.com>
2025-11-26 20:48:43 +0100

    fitst

---
+++ src/main/java/com/example/avecmaven/AvecMavenApplication.java
@@ -0,0 +1,14 @@
+package com.example.avecmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvecMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvecMavenApplication.class, args);
+System.out.println("okk");
+	}
+
+}
